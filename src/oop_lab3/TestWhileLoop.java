ecde8ec4a490ccb6de47431305ec0e196f107b9b
Kamonratmeen <kamonart.meen@gmail.com>
2017-12-12 10:43:50 +0700

    Test While Loop

---
+++ src/oop_lab3/TestWhileLoop.java
@@ -0,0 +1,20 @@
+package oop_lab3;
+
+public class TestWhileLoop {
+    public static void main (String [] args){
+//      while
+          int i = 1;
+          while (i<=50){
+              System.out.println(i+" ");
+               if (i%10==0)
+                   System.out.println("Hello");
+               i++;
+
+          }//while
+
+
+
+
+
+    }
+}//class
