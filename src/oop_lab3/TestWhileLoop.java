a6fd4fae2b2a2a59e37aa2e42d2351640617f9bb
Kamonratmeen <kamonart.meen@gmail.com>
2017-12-12 10:51:36 +0700

    Test While Loop2

--- src/oop_lab3/TestWhileLoop.java
+++ src/oop_lab3/TestWhileLoop.java
@@ -11,6 +11,12 @@ public class TestWhileLoop {
                i++;
 
           }//while
+//       do-while
+          int j=1;
+          do {
+              System.out.println(j + " ");
+              j++;
+          } while (j <=10);
 
 
 
---
+++ src/oop_lab3/TestdowhileLoop.java
@@ -0,0 +1,26 @@
+package oop_lab3;
+
+public class TestdowhileLoop {
+    public static void main (String [] args){
+
+//       do-while
+        int j=1;
+        do {
+            System.out.println(j + " ");
+            j++;
+        } while (j <=10);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}//class
