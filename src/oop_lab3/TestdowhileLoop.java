152f5bb8938fbbb50cc9f220abf34f5cc56816a2
Kamonratmeen <kamonart.meen@gmail.com>
2017-12-12 10:57:35 +0700

    Test dowhile Loop

--- src/oop_lab3/TestdowhileLoop.java
+++ src/oop_lab3/TestdowhileLoop.java
@@ -7,6 +7,8 @@ public class TestdowhileLoop {
         int j=1;
         do {
             System.out.println(j + " ");
+            if (j%2 !=0)
+                System.out.println("Hello");
             j++;
         } while (j <=10);
 
