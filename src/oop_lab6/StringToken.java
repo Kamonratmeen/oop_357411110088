a9fb2628a6e4b9e896faf1dd79506535573a3145
Kamonratmeen <kamonart.meen@gmail.com>
2017-12-27 18:27:39 +0700

    StringToken

---
+++ src/oop_lab6/StringToken.java
@@ -0,0 +1,18 @@
+package oop_lab6;
+
+import java.util.StringTokenizer;
+
+public class StringToken {
+    public static void main(String[] args) {
+
+        String msg = "Hello, RMUTSV Puriwat Lertkai";
+        StringTokenizer myToken = new StringTokenizer(msg);
+        System.out.println(myToken.countTokens());
+
+        while (myToken.hasMoreElements()){
+            StringBuffer myBuf = new StringBuffer(myToken.nextToken());
+            System.out.print(myBuf.reverse()+" ");
+        }//while
+
+    }//main
+}//class
