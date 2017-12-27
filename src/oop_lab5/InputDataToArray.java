e85d1d79341ac7502393b5ef9c950feaf041df0b
Kamonratmeen <kamonart.meen@gmail.com>
2017-12-27 16:06:05 +0700

    Sorting data in array

--- src/oop_lab5/InputDataToArray.java
+++ src/oop_lab5/InputDataToArray.java
@@ -4,22 +4,60 @@ package oop_lab5;
 //3.find average value of data in array
 //4.find maximum value of data in array
 //5.find maximum value of data in array
+//6.Sorting data with Descending Order
+//7.Sorting data whit Ascending Order ข้อมูลจากมากไปน้อย
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class InputDataToArray {
-    private static final int MAX = 10;
+    private static final Integer MAX = 10;
 
     public static void main(String[] args) {
-        int num[] = new int[MAX];
+        Integer num[] = new Integer[MAX];
         num = inputData(num);
         showData(num);
+        summation(num);
+        findMax(num);
+        findMin(num);
+        DescendingOrder(num);
+        AscendingOrder(num);
 
     }//main
 
-    private static void showData(int[] num) {
-        System.out.println("Data in array: ");
+    private static void AscendingOrder(Integer[] num) {
+        Arrays.sort(num, Collections.reverseOrder());
+        showData(num);
+    }//AscendingOrder เรียงข้อมูลจากมากไปน้อย
+
+    private static void DescendingOrder(Integer[] num) {
+        Arrays.sort(num);
+        System.out.println("Descending Order");
+        showData(num);
+    }//DescendingOrder
+
+    private static void findMin(Integer[] num) {
+        System.out.println("The maximum value is: "+(Collections.max(Arrays.asList(num))));
+    }//findMin
+
+    private static void findMax(Integer[] num) {
+        System.out.println("The maximum value is: "+(Collections.max(Arrays.asList(num))));
+    }//findMax
+    private static void summation(Integer[] num) {
+
         int total = 0;
+        for (int i=0;i<num.length;i++){
+            total += num[i];
+        }
+        System.out.println("\nThe summation of " + " Value in array is: "+total);
+        System.out.println("The average value " + " in array is: "+total/MAX);
+
+    }//summation
+
+    private static void showData(Integer[] num) {
+        System.out.println("Data in array: ");
+        Integer total = 0;
         for (int i=0;i<num.length;i++) {
             total += num[i];
         }
@@ -30,7 +68,7 @@ public class InputDataToArray {
 
 
 
-    private static int[] inputData(int []num) {
+    private static Integer[] inputData(Integer []num) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Plese enter data to array: ");
         for (int i=0;i<num.length;i++){
