da2664dfe36d5bf41dd636eb2501d02e123dfe4a
Kamonrat <kamonart.meen>
2018-01-30 10:00:05 +0700

    Class and Object :mySuperCarApp

--- src/oop_lab8/PersonInfo.java
+++ src/oop_lab8/PersonInfo.java
@@ -4,7 +4,10 @@ public class PersonInfo {
     public static void main(String[] args) {
         Student student = new Student("1111111111111","Boy Saiyai","Male",20,"222222222222","IS:Management Technology");
         System.out.println(student.toString());
+
+        System.out.println(student.getName());
     }
+
 
 
 
