f62c211850c6aa99236158abd90ebd4b697ea296
Kamonrat <kamonart.meen>
2018-01-30 09:55:23 +0700

    Class and Object : SuperCar Class

---
+++ src/oop_lab8/PersonInfo.java
@@ -0,0 +1,11 @@
+package oop_lab8;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        Student student = new Student("1111111111111","Boy Saiyai","Male",20,"222222222222","IS:Management Technology");
+        System.out.println(student.toString());
+    }
+
+
+
+}
