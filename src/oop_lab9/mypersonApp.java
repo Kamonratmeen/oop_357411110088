619bf445586fe28225b55385156f68e9e5144e4b
Kamonratmeen <kamonart.meen@gmail.com>
2018-02-06 11:08:34 +0700

    Job

--- src/oop_lab9/mypersonApp.java
+++ src/oop_lab9/mypersonApp.java
@@ -8,6 +8,8 @@ public class mypersonApp {
         Person person = new personID("1111111","Kamonrat Hemtanon",new Address("246/1 M.8","Nakornsithammarat","80240"),new Job("Student","50000"));
         System.out.println(person.toString());
 
+        person.
+
 
 
 
